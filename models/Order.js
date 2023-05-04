2fbe87c36a1f6b559a55088ec3cfc0236a9933d8
Khaled Walead Abu Eriban <91320064+khaled50k@users.noreply.github.com>
2023-05-04 06:40:37 -0700

    Update Order.js

--- models/Order.js
+++ models/Order.js
@@ -44,6 +44,7 @@ Order.virtual('amount').get(function() {
   // Loop over the products in the order and add up the total price
   for (let i = 0; i < this.products.length; i++) {
     const product = this.products[i];
+    console.log(product);
     const discount = product.id.discount || 0; // default to 0 if no discount
     const price = product.id.price * (1 - discount / 100); // apply discount
     total_price += price * product.quantity;
