78d60052279b9bde762c8b980684d0e810136cb9
Khaled Walead Abu Eriban <91320064+khaled50k@users.noreply.github.com>
2023-05-04 02:04:53 -0700

    Update Order.js

--- models/Order.js
+++ models/Order.js
@@ -8,7 +8,7 @@ const Order = mongoose.Schema(
     },
     products: [
       {
-        productId: {
+        id: {
           type: mongoose.Schema.Types.ObjectId,
           ref: "Product",
           required: true,
