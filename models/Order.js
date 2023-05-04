a281b589d26258b66d47eef474f4955bca81d1bb
Khaled Walead Abu Eriban <91320064+khaled50k@users.noreply.github.com>
2023-05-04 02:15:41 -0700

    Update Order.js

--- models/Order.js
+++ models/Order.js
@@ -34,7 +34,21 @@ const Order = mongoose.Schema(
   },
   {
     timestamps: true,
+     toJSON: {
+    virtuals: true // enable virtual properties in JSON output
+  }
   }
 );
+Order.virtual('amount').get(function() {
+  let total_price = 0;
+
+  // Loop over the products in the order and add up the total price
+  for (let i = 0; i < this.products.length; i++) {
+    const product = this.products[i];
+    total_price += product.id.price * product.quantity;
+  }
+
+  return total_price;
+});
 module.exports = mongoose.model("Order", Order);
 
