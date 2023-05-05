5b5ddd9b552bb371535c2bfd32308cb4c5753738
Khaled Walead Abu Eriban <91320064+khaled50k@users.noreply.github.com>
2023-05-04 23:09:32 -0700

    Update user.js

--- routes/user.js
+++ routes/user.js
@@ -8,15 +8,18 @@ const CryptoJS = require("crypto-js");
 
 const router = require("express").Router();
 
+// Update user by id
 router.put("/:id", verifyTokenAndAuthorization, async (req, res) => {
+  // If the request body contains a password, encrypt it
   if (req.body.password) {
     req.body.password = CryptoJS.AES.encrypt(
       req.body.password,
       process.env.PASS_SEC
     ).toString();
   }
-console.log(req.body.password);
+
   try {
+    // Update user in the database and return the updated user
     const updatedUser = await User.findByIdAndUpdate(
       req.params.id,
       {
@@ -29,6 +32,8 @@ console.log(req.body.password);
     res.status(500).json(err);
   }
 });
+
+// Delete user by id
 router.delete("/:id", verifyTokenAndAuthorization, async (req, res) => {
   try {
     await User.findByIdAndRemove(req.params.id);
@@ -37,8 +42,11 @@ router.delete("/:id", verifyTokenAndAuthorization, async (req, res) => {
     res.status(500).json(error);
   }
 });
+
+// Get user by id (only for admins)
 router.get("/find/:id", verifyTokenAndAdmin, async (req, res) => {
   try {
+    // Find user by id and exclude password from the returned data
     const user = await User.findById(req.params.id);
     const { password, ...data } = user._doc;
     res.status(200).json(data);
@@ -46,9 +54,13 @@ router.get("/find/:id", verifyTokenAndAdmin, async (req, res) => {
     res.status(500).json(error);
   }
 });
-router.get("/", async (req, res) => {
+
+// Get all users (only for admins)
+router.get("/", verifyTokenAndAdmin, async (req, res) => {
   const query = req.query.new;
   try {
+    // If the "new" query parameter is present, return the 5 newest users
+    // Otherwise, return all users
     const users = query
       ? await User.find().sort({ _id: -1 }).limit(5)
       : await User.find();
@@ -58,12 +70,14 @@ router.get("/", async (req, res) => {
     res.status(500).json(error);
   }
 });
-// get user  status
+
+// Get user statistics (only for admins)
 router.get("/stats", verifyTokenAndAdmin, async (req, res) => {
   const date = new Date();
   const lastYear = new Date(date.setFullYear(date.getFullYear() - 1));
 
   try {
+    // Aggregate user data by month for the last year
     const data = await User.aggregate([
       { $match: { createdAt: { $gte: lastYear } } },
       {
@@ -78,9 +92,10 @@ router.get("/stats", verifyTokenAndAdmin, async (req, res) => {
         },
       },
     ]);
-    res.status(200).json(data)
+    res.status(200).json(data);
   } catch (err) {
     res.status(500).json(err);
   }
 });
+
 module.exports = router;
