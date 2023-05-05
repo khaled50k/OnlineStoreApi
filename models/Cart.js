bf213fd2274ca1a1af3a63a54598d340d900a38d
Khaled Walead Abu Eriban <91320064+khaled50k@users.noreply.github.com>
2023-05-05 08:48:42 -0700

    Update Cart.js

--- models/Cart.js
+++ models/Cart.js
@@ -1,4 +1,6 @@
 const mongoose = require("mongoose");
+const Product = require("./Product.js");
+
 const Cart = mongoose.Schema(
   {
     userId: {
@@ -17,16 +19,31 @@ const Cart = mongoose.Schema(
           type: Number,
           default: 1,
         },
-              totalPrice:{
-                      type:Number,
-                      required:true
-              }
       },
     ],
+
+    amount: {
+      type: Number,
+      default: 0,
+    },
   },
   {
     timestamps: true,
   }
 );
-module.exports = mongoose.model("Cart", Cart);
+Cart.methods.updateAmount = async function () {
+  const products = this.products.map(async (product) => {
+    const p = await Product.findOne({ _id: product.id });
+    const discount = p.discount || 0;
+    const price = p.price * (1 - discount / 100);
+    return price * product.quantity;
+  });
+  const total_price = await Promise.all(products).then((prices) =>
+    prices.reduce((acc, price) => acc + price, 0)
+  );
+  this.amount = total_price;
+  console.log(total_price);
 
+  await this.save();
+};
+module.exports = mongoose.model("Cart", Cart);
