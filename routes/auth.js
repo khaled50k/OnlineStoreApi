6abda4d4a7866b6763880d7272ff8f857605820a
Khaled Walead Abu Eriban <91320064+khaled50k@users.noreply.github.com>
2023-05-04 23:08:35 -0700

    Update auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -1,3 +1,4 @@
+// Import necessary modules and middleware
 const router = require("express").Router();
 const User = require("../models/User");
 const jwt = require("jsonwebtoken");
@@ -9,61 +10,67 @@ const {
   verifyTokenAndAdmin,
 } = require("./verifyToken");
 
-// get user info using cookies
+// Route to get user info using cookies
 router.post("/", getUserId);
-// Register a new user
+
+// Route to register a new user
 router.post("/register", async (req, res) => {
-  const user = new User({
-    username: req.body.username.toLowerCase(),
-    email: req.body.email,
-    password: CryptoJS.AES.encrypt(
-      req.body.password,
-      process.env.PASS_SEC
-    ).toString(),
-    role: req.body.role ? req.body.role : "user",
-  });
+  const { username, email, password, role } = req.body;
+  const encryptedPassword = CryptoJS.AES.encrypt(
+    password,
+    process.env.PASS_SEC
+  ).toString();
 
   try {
-    const savedUser = await user.save();
-    res.status(201).json(savedUser);
+    // Create a new user and save to database
+    const user = await User.create({
+      username: username.toLowerCase(),
+      email,
+      password: encryptedPassword,
+      role: role || "user",
+    });
+    res.status(201).json(user);
   } catch (error) {
     res.status(500).json(error);
   }
 });
 
-// Log in a user
+// Route to log in a user
 router.post("/login", async (req, res) => {
+  const { username, password } = req.body;
+  try {
+    // Find the user in the database
+    const user = await User.findOne({ username: username.toLowerCase() });
+    if (!user) {
+      // If user is not found, return error
+      return res.status(404).json({ message: "User not found" });
+    }
 
-  const user = await User.findOne(
- { username: req.body.username.toLowerCase() 
-  });
+    // Decrypt user password and check if it matches input
+    const decryptedPassword = CryptoJS.AES.decrypt(
+      user.password,
+      process.env.PASS_SEC
+    ).toString(CryptoJS.enc.Utf8);
 
+    if (decryptedPassword !== password) {
+      // If password is invalid, return error
+      return res.status(400).json({ message: "Invalid password" });
+    }
 
-  if (!user) {
-    return res.status(404).send({ message: "User not found" });
+    // Create a JWT token with user id and role and set as a cookie in response
+    const token = jwt.sign(
+      { userId: user._id, role: user.role },
+      process.env.JWT_SEC
+    );
+    res.cookie("jwt", token, {
+      httpOnly: true,
+      maxAge: 24 * 60 * 60 * 1000, // 1 day
+    });
+    res.status(200).json({ message: "Login successful", token });
+  } catch (error) {
+    res.status(500).json(error);
   }
-
-  const decryptedPassword = CryptoJS.AES.decrypt(
-    user.password,
-    process.env.PASS_SEC
-  ).toString(CryptoJS.enc.Utf8);
-
-  if (decryptedPassword !== req.body.password) {
-    return res.status(400).send({ message:"Invalid password"  });
-  }
-
-  const token = jwt.sign(
-    { id: user._id, role: user.role },
-    process.env.JWT_SEC
-  );
-
-  // Set the JWT token as a cookie in the response
-  res.cookie("jwt", token, {
-    httpOnly: true,
-    maxAge: 24 * 60 * 60 * 1000, // 1 day
-  });
-
-  res.status(200).json({ message: "Login successful", token: token });
 });
 
+// Export router module
 module.exports = router;
